6d6ba1712f45e9df91420dc81d7956fdda9cf680
arthurgbranco <agbranco@sga.pucminas.br>
2021-03-03 21:01:56 -0300

    Better variable naming

--- src/app/Client.java
+++ src/app/Client.java
@@ -9,28 +9,29 @@ public class Client {
     public static void main(String[] args) {
 
         try {
-            String nomeEleitor = "Arthur Gramiscelli Branco";
-            String vote = "135";
+            String eleitor = "Arthur Gramiscelli Branco";
+            String candidato = "LUIZ FERNANDO SANTOS";
 
             if(args.length > 1){
-                nomeEleitor = args[0];
-                vote = args[1];
+                eleitor = args[0];
+                candidato = args[1];
             }else{
                 System.out.println("Args missing, using default values...");
-                System.out.println("Default eleitor: " + nomeEleitor);
-                System.out.println("Default vote: " + vote);
+                System.out.println("Default eleitor: " + eleitor);
+                System.out.println("Default candidato: " + candidato);
             }
 
             // Gera o hash MD5 baseado no nome do eleitor
             MessageDigest md = MessageDigest.getInstance("MD5");
-            md.update(nomeEleitor.getBytes());
+            md.update(eleitor.getBytes());
             byte[] md5 = md.digest();
 
             // Formata em Hexadecimal minusculo para exibir na tela
             BigInteger numMd5 = new BigInteger(1, md5);
             String hashMd5 = String.format("%022x", numMd5);
 
-            System.out.println("Nome: " + nomeEleitor + System.lineSeparator() + "MD5: " + hashMd5);
+            System.out.println("Nome: " + eleitor + System.lineSeparator()
+                    + "MD5: " + hashMd5 + System.lineSeparator() +"Candidato: " + candidato);
         } catch (NoSuchAlgorithmException e) {
             throw new RuntimeException(e);
         }
